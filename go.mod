9e951217c91f112638fdc64233f5201b8265c90b
dollarkillerx <adapawang@gmail.com>
2019-11-17 09:06:49 +0800

    init go micro hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dollarkillerx/GoMicro-Study
+
+require github.com/micro/protoc-gen-micro v1.0.0 // indirect
+
+go 1.13
---
+++ server/server_main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"context"
+	hello "github.com/micro/go"
+)
+
+type Say struct {}
+
+func (s *Say) Hello(ctx context.Context,req *)
